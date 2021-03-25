ad60fe71c53196b09a1728c7bd7cbdc8e5f50b1a
efef10 <co.efrat10@gmail.com>
2021-03-25 12:02:56 +0200

    start from schema instead of config

--- src/controllers/dataCovertionsController.js
+++ src/controllers/dataCovertionsController.js
@@ -2,7 +2,7 @@ import {dataCovertorService} from '../services';
 
 const getFullDataJson = async (req, res) => {
     try {
-        res.json({data: await dataCovertorService.getFullDataJson(req.body)});
+        res.json({response: await dataCovertorService.getFullDataJson(req.body)});
     }
     catch(err){
         res.send(JSON.stringify(err));
--- src/services/dataCovertorService.js
+++ src/services/dataCovertorService.js
@@ -1,35 +1,63 @@
 import {fileReaderRepository} from '../repositories'
 
 const generateValue = (fieldKey, schema, data, isRequired, isVisible) => {
-    let json = {};
-    if(schema && schema[fieldKey])
-        json = JSON.parse(schema[fieldKey]);
-    
+    const json = JSON.parse(schema[fieldKey]);
     json.value = (data && (data[fieldKey] || ""));
     json.required = isRequired;
     json.visibility = isVisible; 
     return JSON.stringify(json);
 }
 
-const recursiveFindData = (keysArr, schema, data, acc, isRequired, isVisible) => {
-    const fieldKey = keysArr[0];
+const getConfigAsObject = configArr => {
+    return configArr.reduce((acc, conf) => (
+        {
+            ...acc,
+            [conf.fieldKey]: {
+                required: conf.require,
+                visible: conf.visible,
+            }
+        }
+    ),{})
+}
 
-    if(keysArr.length === 1){
+const isEmptyObject = obj => obj && Object.keys(obj).length === 0 && obj.constructor === Object
+
+const shouldNotDisplayRecord = configData => configData && !configData.visible
+
+const recursiveBuildData = (fieldKey, schema, config, data, acc) => {
+
+    const fieldKeysAsArray = fieldKey.split('.');
+    const currentFieldKey = fieldKeysAsArray[fieldKeysAsArray.length -1];
+
+    if(typeof schema[currentFieldKey] === "string"){
+        const configData = config[fieldKey]
+
+        if(shouldNotDisplayRecord(configData))
+            return {...acc}
         return {
-           [fieldKey]: generateValue(fieldKey, schema, data, isRequired, isVisible),
+            ...acc,
+            [currentFieldKey]: generateValue(currentFieldKey, 
+                                            schema, 
+                                            data, 
+                                            configData && configData.required, 
+                                            configData && configData.visible)
         }
     }
-    return {
-        [fieldKey]: {
-            ...acc[fieldKey],
-            ...recursiveFindData(keysArr.slice(1), 
-                                schema && schema[fieldKey], 
-                                data && data[fieldKey], 
-                                acc && acc[fieldKey], 
-                                isRequired, 
-                                isVisible)
+
+    const subAggregatedData = 
+    Object.keys(schema && schema[currentFieldKey])
+    .reduce((subAcc, subFieldKey)=> {
+        return {
+            ...subAcc,
+            ...recursiveBuildData(`${fieldKey}.${subFieldKey}`, schema[currentFieldKey], config, data && data[currentFieldKey], subAcc)
         }
-       
+    }, {})
+
+    if (isEmptyObject(subAggregatedData))
+        return {...acc}
+    return {
+        ...acc,
+        [currentFieldKey]: subAggregatedData
     }
 };
 
@@ -37,14 +65,14 @@ const getFullDataJson = async (data) => {
     try{
         const config = await fileReaderRepository.readFromJson("config");
         const schema = await fileReaderRepository.readFromJson("schema");
-        return config
-            .filter(conf => conf.visible)
-            .reduce((acc, conf) => {
-                return {
-                    ...acc, 
-                    ...recursiveFindData(conf.fieldKey.split('.'), schema, data, acc, conf.require, conf.visible),
-                } 
-            }, {});
+
+        return Object.keys(schema)
+        .reduce((acc, fieldKey) => {
+            return {
+                ...acc,
+                ... recursiveBuildData(fieldKey, schema, getConfigAsObject(config), data, {})
+            }
+        }, {});
     }
     catch(err){
         throw `failed to get data, error: ${err}`;
