adc010cf8921f7ef1536e99ec8e0a2f7fd575f2d
efef10 <co.efrat10@gmail.com>
2021-03-25 09:32:17 +0200

    fix missing field in data or schema

--- src/services/dataCovertorService.js
+++ src/services/dataCovertorService.js
@@ -4,15 +4,13 @@ import {fileReaderRepository} from '../repositories'
 
 const recursiveFindData = (keysArr, schema, data, acc, isRequired, isVisible) => {
     const fieldKey = keysArr[0];
-    if(fieldKey === "propertyAddress" || fieldKey === "updatedAt"){
-        console.log(keysArr, schema, data, acc, isRequired)
-    }
+
     if(keysArr.length === 1){
         let json = {};
-        if(schema[fieldKey])
+        if(schema && schema[fieldKey])
             json = JSON.parse(schema[fieldKey]);
         
-        json.value = (data[fieldKey] || "");
+        json.value = (data && (data[fieldKey] || ""));
         json.required = isRequired;
         json.visibility = isVisible; 
 
@@ -23,7 +21,12 @@ const recursiveFindData = (keysArr, schema, data, acc, isRequired, isVisible) =>
     return {
         [fieldKey]: {
             ...acc[fieldKey],
-            ...recursiveFindData(keysArr.slice(1), schema[fieldKey], data[fieldKey], acc[fieldKey], isRequired, isVisible)
+            ...recursiveFindData(keysArr.slice(1), 
+                                schema && schema[fieldKey], 
+                                data && data[fieldKey], 
+                                acc && acc[fieldKey], 
+                                isRequired, 
+                                isVisible)
         }
        
     }
@@ -34,8 +37,7 @@ const getFullDataJson = async (data) => {
         const config = await fileReaderRepository.readFromJson("config");
         const schema = await fileReaderRepository.readFromJson("schema");
         return config
-        .filter(conf=> conf.fieldKey.startsWith("relatedProperty"))
-            // .filter(conf => conf.visible)
+            .filter(conf => conf.visible)
             .reduce((acc, conf) => {
                 return {
                     ...acc, 
